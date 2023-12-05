d649c2c35e232ae9b604125da96a73a54eb0ed54
hoanganht91 <hoanganht91@gmail.com>
2023-12-05 10:49:46 +0000

    Add coordinate output

--- scripts/api.py
+++ scripts/api.py
@@ -51,7 +51,41 @@ def rembg_api(_: gr.Blocks, app: FastAPI):
         )
 
         return {"image": api.encode_pil_to_base64(image).decode("utf-8")}
-    
+
+
+    @app.post("/sdapi/v2/rembg")
+    async def rembg_remove_v2(
+        input_image: str = Body("", title='rembg input image'),
+        model: str = Body("u2net", title='rembg model'), 
+        return_mask: bool = Body(False, title='return mask'), 
+        alpha_matting: bool = Body(False, title='alpha matting'), 
+        alpha_matting_foreground_threshold: int = Body(240, title='alpha matting foreground threshold'), 
+        alpha_matting_background_threshold: int = Body(10, title='alpha matting background threshold'), 
+        alpha_matting_erode_size: int = Body(10, title='alpha matting erode size')
+    ):
+        if not model or model == "None":
+            return
+
+        input_image = api.decode_base64_to_image(input_image)
+
+        image = rembg.remove(
+            input_image,
+            session=rembg.new_session(model),
+            only_mask=return_mask,
+            alpha_matting=alpha_matting,
+            alpha_matting_foreground_threshold=alpha_matting_foreground_threshold,
+            alpha_matting_background_threshold=alpha_matting_background_threshold,
+            alpha_matting_erode_size=alpha_matting_erode_size,
+        )
+        left, upper, right, lower = image.getbbox()
+
+        return  {
+                    "image": api.encode_pil_to_base64(image).decode("utf-8"),
+                    "box": {
+                        "x": left, "y" : upper, "width" : right - left, "height" : lower - upper,
+                    }
+                }
+
     
     def crop_and_resize_image(image, width=None, height=None):
         print("Cropping and resizing image", width, height)
